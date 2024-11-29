3114a5c2ac2ab6767142ffaef336c8485d024ac6
Jerry Peng <jerrypeng0112@hotmail.com>
2024-11-28 17:13:09 -0800

    Add private note in codepull.sh

--- scripts/codepull.sh
+++ scripts/codepull.sh
@@ -6,5 +6,8 @@ mkdir -p ~/Files/Codes
 echo "Cloning notes"
 git clone git@github.com:jerry-peng/notes.git ~/Files/Codes/notes
 
+echo "Cloning private notes"
+git clone git@github.com:jerry-peng/private-notes.git ~/Files/Codes/private-notes
+
 echo "Cloning osx-boostrap"
 git clone git@github.com:jerry-peng/osx-bootstrap.git ~/Files/Codes/osx-bootstrap
